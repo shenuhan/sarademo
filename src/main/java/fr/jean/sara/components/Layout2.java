32dcedcbc603b608bdcb0fd26fa6bef6457b6186
jean.haderer@gmail.com <jean.haderer@gmail.com>
2016-02-10 23:38:17 +0100

    ???

--- src/main/java/fr/jean/sara/components/Layout2.java
+++ src/main/java/fr/jean/sara/components/Layout2.java
@@ -1,9 +1,11 @@
 package fr.jean.sara.components;
 
+import org.apache.tapestry5.annotations.Import;
 
 /**
  * Layout component for pages of application test-project.
  */
+@Import(module="jquery")
 public class Layout2
 {
 }
